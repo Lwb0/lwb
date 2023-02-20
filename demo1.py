0317753793a7a52953d2ff04f8297ef2b2529077
LWB <lwb866823@163.com>
2023-02-20 15:06:44 +0800

    add demo1.py

---
+++ demo1.py
@@ -0,0 +1,10 @@
+# 选择法排序
+num = [4, 45, 12, 456, 1, 53, 21]
+for i in range(len(num)-1):
+    for j in range(len(num)-1-i):
+        if num[i] > num[j+i+1]:
+            temp = num[i]
+			num[i] = num[j+i+1]
+            num[j+1+i] = temp
+			
+print(num)
